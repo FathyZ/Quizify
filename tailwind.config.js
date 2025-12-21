965a9e77ca5c923230c86208baaa381b121809b0
ZiadHesham0 <zyadhesham861@gmail.com>
2025-12-21 22:52:31 +0200

    try push

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './pages/**/*.html',
+    './src/**/*.{html,js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('tailwind-scrollbar'),
+
+  ],
+}
