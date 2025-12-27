c7e24ecfae1c90deba698482510f874893bcacfa
ZiadHesham0 <zyadhesham861@gmail.com>
2025-12-27 21:54:18 +0200

    working on sidebar

--- js/exam.js
+++ js/exam.js
@@ -14,7 +14,7 @@ let questions = [
   {
     id: 2,
     text: "What is the time complexity of accessing an element in an array?",
-    status: "marked", //  none - Flagged
+    status: "none", //  none - Flagged
     selectedAnswerId: null,
     answers: [
       { id: 1, text: "O(1)", isCorrect: true },
@@ -101,25 +101,32 @@ let currentIndex = 0;
 const flagBtn = document.getElementById("flagBtn");
 const nextBtn = document.getElementById("nextBtn");
 const prevBtn = document.getElementById("prevBtn");
+const hamburgerBtn = document.getElementById("hamburgerBtn");
+const navigators = document.querySelectorAll(".navigator");
+const flagIcon = document.getElementById("flagIcon");
+const closeIcon = document.getElementById("closeIcon");
+const sidebar = document.getElementById("sidebar");
+const overlay = document.getElementById("overlay");
+const questionNavigatorContainer =
+  document.getElementById("navigatorContainer");
 const questionContent = document.getElementById("questionContent");
 const answersContainer = document.getElementById("answers");
-const flaggedQuestionsContainer = document.getElementById(
-  "flaggedQuestionsContainer"
-);
+// const flaggedQuestionsContainer = document.getElementById(
+//   "flaggedQuestionsContainer"
+// );
 const progressBar = document.getElementById("progressBar");
 let progressBarWidth = 10;
 
 // Functions Must Run at the start of the Exam
 updateProgressBar();
 displayQuestions();
-displayFlaggedQuestions();
+// displayFlaggedQuestions();
 // if (localStorage.getItem("userAnswers") != null) {
 //     userAnswers = JSON.parse(localStorage.getItem("userAnswers"));
 //     displayQuestions(userAnswers);
 // }
 // updateTimer();
 /////////////////////////////////////////////////////////////////////
-
 
 // Update next/prev Buttons Status
 function updateBtnsState() {
@@ -156,108 +163,198 @@ prevBtn.addEventListener("click", function () {
 
 // Flag/unflag question
 flagBtn.addEventListener("click", function () {
-  if ((questions[currentIndex].status = "flagged")) {
-    questions[currentIndex].status = "none";
-  } else {
+  if (questions[currentIndex].status == "none") {
     questions[currentIndex].status = "flagged";
+  } else {
+    questions[currentIndex].status = "none";
   }
   displayFlaggedQuestions();
+
+  displayQuestions();
 });
+
 // Display Flagged Questions
 function displayFlaggedQuestions() {
-  // let flaggedQuestion = document.createElement("div")
-  // flaggedQuestion.innerHTML = ``
-  // flaggedQuestionsContainer.appendChild;
+  console.log(navigators[currentIndex].innerText);
+  if (questions[currentIndex].status == "flagged") {
+    navigators[currentIndex].style.cssText = `
+      color: var(--color-orange-400) ;
+    border : 1px solid var(--color-orange-300) ;
+    background-color: var(--color-orange-50)  ;`;
+  } else if (questions[currentIndex].status == "none") {
+    navigators[currentIndex].style.cssText = `
+      color: var(--color-gray-900) ;
+    border : 1px solid var(--color-gray-300) ;
+    background-color: var(--color-gray-50)  ;
+    `;
+    displayAnsweredQuestions(); //if it is unflagged check if it is already answered to make it green
+  }
+
+  // questions.forEach((question) => {
+  //   if (question.status == "flagged") {
+  //     navigators[question.id - 1].style.cssText = `
+  //     color: var(--color-orange-400) ;
+  //   border : 1px solid var(--color-orange-300) ;
+  //   background-color: var(--color-orange-50)  ;`;
+  //   }
+  //   else if (question.status == "none") {
+  //     navigators[question.id - 1].style.cssText = `
+  //     color: var(--color-gray-900) ;
+  //   border : 1px solid var(--color-gray-300) ;
+  //   background-color: var(--color-gray-50)  ;
+  //   `;
+  //   }
+  // });
 }
 
 // Display Questions And its Answers
 function displayQuestions() {
-  // console.log(questions[currentIndex].text);
+  // toggle flag icon to indicate if question is flagged or not
+  if (questions[currentIndex].status == "flagged") {
+    flagIcon.classList.replace("fa-regular", "fa-solid");
+  } else if (questions[currentIndex].status == "none") {
+    flagIcon.classList.replace("fa-solid", "fa-regular");
+  }
   questionContent.innerText = questions[currentIndex].text;
-  // answerContent1.innerText = questions[currentIndex].answers[0].text;
-  // answerContent2.innerText = questions[currentIndex].answers[1].text;
-  // answerContent3.innerText = questions[currentIndex].answers[2].text;
-  // answerContent4.innerText = questions[currentIndex].answers[3].text;
+  // see if user choose an answer for this question so make it checked
+  const selectedAnswerId = userAnswers[currentIndex];
+
   answersContainer.innerHTML = `<!-- First Answer -->
             <label
-              for="q${currentIndex+1}-answer1"
+              for="q${currentIndex + 1}-answer1"
               class="flex items-center gap-4 my-2 bg-gray-100 rounded-lg border border-gray-300 lg:p-4 p-3 lg:text-sm text-xs shadow-sm transition-colors hover:bg-gray-50 has-checked:text-secondary has-checked:border-primary has-checked:ring-1 has-checked:ring-primary"
             >
               <input
                 type="radio"
-                name="q${currentIndex+1}"
+                name="q${currentIndex + 1}"
                 value="0"
-                id="q${currentIndex+1}-answer1"
+                id="q${currentIndex + 1}-answer1"
                 class="lg:size-5 size-3 border-gray-300"
+                ${selectedAnswerId === "0" ? "checked" : ""}
               />
-              <p id="answerContent1">${questions[currentIndex].answers[0].text}</p>
+              <p id="answerContent1">${
+                questions[currentIndex].answers[0].text
+              }</p>
             </label>
 
             <!-- Second Answer -->
             <label
-              for="q${currentIndex+1}-answer2"
+              for="q${currentIndex + 1}-answer2"
               class="flex items-center gap-4 my-2 bg-gray-100 rounded-lg border border-gray-300 lg:p-4 p-3 lg:text-sm text-xs shadow-sm transition-colors hover:bg-gray-50 has-checked:text-secondary has-checked:border-primary has-checked:ring-1 has-checked:ring-primary"
             >
               <input
                 type="radio"
-                name="q${currentIndex+1}"
+                name="q${currentIndex + 1}"
                 value="1"
-                id="q${currentIndex+1}-answer2"
+                id="q${currentIndex + 1}-answer2"
                 class="lg:size-5 size-3 border-gray-300"
+                ${selectedAnswerId === "1" ? "checked" : ""}
               />
-              <p id="answerContent2">${questions[currentIndex].answers[1].text}</p>
+              <p id="answerContent2">${
+                questions[currentIndex].answers[1].text
+              }</p>
             </label>
 
             <!-- Third Answer -->
             <label
-              for="q${currentIndex+1}-answer3"
+              for="q${currentIndex + 1}-answer3"
               class="flex items-center gap-4 my-2 bg-gray-100 rounded-lg border border-gray-300 lg:p-4 p-3 lg:text-sm text-xs shadow-sm transition-colors hover:bg-gray-50 has-checked:text-secondary has-checked:border-primary has-checked:ring-1 has-checked:ring-primary"
             >
               <input
                 type="radio"
-                name="q${currentIndex+1}"
+                name="q${currentIndex + 1}"
                 value="2"
-                id="q${currentIndex+1}-answer3"
+                id="q${currentIndex + 1}-answer3"
                 class="lg:size-5 size-3 border-gray-300"
+                ${selectedAnswerId === "2" ? "checked" : ""}
               />
-              <p id="answerContent3">${questions[currentIndex].answers[2].text}</p>
+              <p id="answerContent3">${
+                questions[currentIndex].answers[2].text
+              }</p>
             </label>
 
             <!-- Fourth Answer -->
             <label
-              for="q${currentIndex+1}-answer4"
+              for="q${currentIndex + 1}-answer4"
               class="flex items-center gap-4 my-2 bg-gray-100 rounded-lg border border-gray-300 lg:p-4 p-3 lg:text-sm text-xs shadow-sm transition-colors hover:bg-gray-50 has-checked:text-secondary has-checked:border-primary has-checked:ring-1 has-checked:ring-primary"
             >
               <input
                 type="radio"
-                name="q${currentIndex+1}"
+                name="q${currentIndex + 1}"
                 value="3"
-                id="q${currentIndex+1}-answer4"
+                id="q${currentIndex + 1}-answer4"
                 class="lg:size-5 size-3 border-gray-300"
+                ${selectedAnswerId === "3" ? "checked" : ""}
               />
-              <p id="answerContent4">${questions[currentIndex].answers[3].text}</p>
+              <p id="answerContent4">${
+                questions[currentIndex].answers[3].text
+              }</p>
             </label>`;
 }
 
 // Update Progress Bar Width
 function updateProgressBar() {
   progressBarWidth = currentIndex * 10;
-  console.log(`w-[${progressBarWidth}%]`);
   progressBar.style.width = progressBarWidth + "%";
 }
 
-
-
 // Using Event Bubbling on the Answers Container--> if any thing inside the container changed get what changed
-answersContainer.addEventListener("change" , function(e){
-  let answer = e.target ; // to get the radio that has been changed 
+answersContainer.addEventListener("change", function (e) {
+  let answer = e.target; // to get the radio that has been changed
   console.log(answer.value);
   userAnswers[currentIndex] = answer.value;
-  saveAnswers()
-})
-
+  saveAnswers();
+  displayAnsweredQuestions();
+});
 
 // Update user answers in local storage
-function saveAnswers(){
+function saveAnswers() {
   localStorage.setItem("userAnswers", JSON.stringify(userAnswers));
 }
+
+// Using Event Bubbling on the Navigator Container -> if question Pressed inside the container go to that question
+questionNavigatorContainer.addEventListener("click", function (e) {
+  if (e.target.tagName === "BUTTON") {
+    currentIndex = e.target.innerText - 1;
+    updateBtnsState();
+    updateProgressBar();
+    displayQuestions();
+    // close sidebar if opened
+    sidebar.classList.add("hidden");
+    closeIcon.classList.add("hidden");
+    overlay.classList.add("hidden");
+  }
+});
+
+// Coloring answered question in the question navigator
+function displayAnsweredQuestions() {
+  if (questions[currentIndex].status != "flagged") {
+    if (userAnswers[currentIndex] != null) {
+      navigators[currentIndex].style.cssText = `
+        color: var(--color-green-900) ;
+        border : 1px solid var(--color-green-300) ;
+        background-color: var(--color-green-100)  ;
+        `;
+    }
+  }
+}
+
+// Open sidebar
+hamburgerBtn.addEventListener("click", function () {
+  sidebar.classList.remove("hidden");
+  closeIcon.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+});
+
+// Close Sidebar
+closeIcon.addEventListener("click", function () {
+  sidebar.classList.add("hidden");
+  closeIcon.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
+
+overlay.addEventListener("click", function () {
+  sidebar.classList.add("hidden");
+  closeIcon.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
